15de9a81ae6044c7cde6f5a8f937eb770686a8f7
topcue <topcue1019@gmail.com>
2026-01-18 09:00:18 +0000

    Fix bug

--- fasteval.py
+++ fasteval.py
@@ -16,16 +16,15 @@ class FunctionDataset_Fast(torch.utils.data.Dataset):
 		self.arr2 = arr2
 		assert(len(arr1) == len(arr2))
 
-
-    def __getitem__(self, idx):
+	def __getitem__(self, idx):
 		return self.arr1[idx].squeeze(0), self.arr2[idx].squeeze(0)
 
 
-    def __len__(self):
+	def __len__(self):
 		return len(self.arr1)
 
 
-def eval_O(ebds, TYPE1, TYPE2):
+def eval_O(ebds, TYPE1, TYPE2, device, POOLSIZE=32):
 	funcarr1 = []
 	funcarr2 = []
 
@@ -61,20 +60,21 @@ def eval_O(ebds, TYPE1, TYPE2):
 				else:
 					Recall_AT_1.append(0)
 				SIMS.append(1.0 / posi)
+
 	print(TYPE1, TYPE2, 'MRR{}: '.format(POOLSIZE), np.array(SIMS).mean())
 	print(TYPE1, TYPE2, 'Recall@1: ', np.array(Recall_AT_1).mean())
 	return np.array(Recall_AT_1).mean()
 
 
 def main():
-    parser = argparse.ArgumentParser(description="jTrans-FastEval")
+	parser = argparse.ArgumentParser(description="jTrans-FastEval")
 	parser.add_argument("--experiment_path", type=str, default='./experiments/BinaryCorp-3M/jTrans.pkl', help="experiment to be evaluated")
 	parser.add_argument("--poolsize", type=int, default=32, help="size of the function pool")
 	parser.add_argument("--device", type=str, default="cpu")
 	args = parser.parse_args()
 
 	device = torch.device(args.device)
-    #! Pool size defines the retrieval evaluation difficulty and directly affects metrics.
+	#! Pool size defines the retrieval evaluation difficulty and directly affects metrics.
 	POOLSIZE = args.poolsize
 	ff = open(args.experiment_path,'rb')
 	ebds = pickle.load(ff)
@@ -82,12 +82,12 @@ def main():
 
 	print(f'evaluating... poolsize={POOLSIZE}')
 
-	eval_O(ebds,'O0','O3')
-	eval_O(ebds,'O0','Os')
-	eval_O(ebds,'O1','Os')
-	eval_O(ebds,'O1','O3')
-	eval_O(ebds,'O2','Os')
-	eval_O(ebds,'O2','O3')
+	eval_O(ebds, 'O0', 'O3', device, POOLSIZE)
+	eval_O(ebds, 'O0', 'Os', device, POOLSIZE)
+	eval_O(ebds, 'O1', 'Os', device, POOLSIZE)
+	eval_O(ebds, 'O1', 'O3', device, POOLSIZE)
+	eval_O(ebds, 'O2', 'Os', device, POOLSIZE)
+	eval_O(ebds, 'O2', 'O3', device, POOLSIZE)
 
 
 if __name__ == '__main__':
