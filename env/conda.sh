5b0fed80a74941d1d657584c85f3fb69386df7e5
topcue <topcue1019@gmail.com>
2026-01-19 06:52:02 +0000

    Cleanup

--- env/conda.sh
+++ env/conda.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
-SCRIPT_PATH="$(readlink -f "$0")" 		# jTrans/env/conda.sh
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"	# jTrans/env
+SCRIPT_PATH="$(readlink -f "$0")"       # jTrans/env/conda.sh
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"  # jTrans/env
 CONDA_DIR="${SCRIPT_DIR}/miniconda3"
 
 source ${CONDA_DIR}/bin/activate
 conda activate jtrans
-
 
 # EOF
